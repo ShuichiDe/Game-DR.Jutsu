9ccb36b9589ab3edfd6e1304bd43b7c2db21b85c
ToanPhuc <85103541+PhucDev2304@users.noreply.github.com>
2024-06-23 19:52:56 +0700

    Update Switch Scene and API question

---
+++ My project/Assets/Scenes/Scripts/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Phương thức để chuyển scene
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
